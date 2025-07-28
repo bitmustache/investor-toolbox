325bb40ea98614858abacae5da541a2fc7dc7966
dmg4ming@gmail.com <dmg4ming@gmail.com>
2025-07-28 13:26:30 +0000

    Edited MainPage.StockSearchComparison

--- client_code/MainPage/StockSearchComparison/__init__.py
+++ client_code/MainPage/StockSearchComparison/__init__.py
@@ -54,6 +54,16 @@ class StockSearchComparison(StockSearchComparisonTemplate):
 
     self.status_label.text = "Fetching data..."
 
+    # Ensure start_date and end_date are datetime.datetime objects
+    import datetime
+    
+    start_date = datetime.datetime.combine(self.stockcomparison_startdate.date, datetime.datetime.min.time())
+    end_date = datetime.datetime.combine(self.stockcomparison_enddate.date, datetime.datetime.min.time())
+
+    print("Start:", self.stockcomparison_startdate.date)
+    print("End:", self.stockcomparison_enddate.date)
+
+    
     try:
       img_base64 = anvil.server.call('get_stock_comparison_graph', stock_1, stock_2, metal, start_date, end_date)
       
